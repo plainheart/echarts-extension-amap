a66ce2c4cf19b5b694a955f9b16a5528fa77ce30
plainheart <yhen@all-my-life.cn>
2021-03-19 14:33:44 +0800

    feat: add type definition for typescript.

---
+++ export.d.ts
@@ -0,0 +1,30 @@
+import { use } from 'echarts/core'
+import EChartsV4 from 'echarts/lib/echarts'
+
+type UnionToIntersection<U> =
+  (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+
+type LastOf<T> =
+  UnionToIntersection<T extends any ? () => T : never> extends () => (infer R) ? R : never
+
+type Push<T extends any[], V> = [...T, V]
+
+type TuplifyUnion<T, L = LastOf<T>, N = [T] extends [never] ? true : false> =
+  true extends N ? [] : Push<TuplifyUnion<Exclude<T, L>>, L>
+
+type EChartsExtensionInstallRegisters = Parameters<TuplifyUnion<Parameters<typeof use>[0]>[0]>[0]
+
+type EChartsRegisters = EChartsExtensionInstallRegisters extends never
+  ? typeof EChartsV4
+  : EChartsExtensionInstallRegisters
+
+/**
+ * To install AMap component
+ * @param registers echarts registers. If using v4, it should be echarts namespace.
+ */
+export declare function install(registers: EChartsRegisters): void
+
+export declare const name = 'echarts-extension-amap'
+export declare const version = '1.9.2'
+
+export default install
---
+++ index.d.ts
@@ -0,0 +1,37 @@
+declare namespace EChartsExtensionAMapComponent {
+  const name = 'echarts-extension-amap'
+  const version = '1.9.2'
+
+  /**
+   * Extended AMap component option
+   */
+  interface AMapComponentOption {
+    /**
+     * The zIndex of echarts layer for AMap.
+     * @default 2000
+     * @deprecated deprecated since v1.9.0, use `echartsLayerInteractive` instead.
+     */
+    echartsLayerZIndex?: number
+    /**
+     * Whether echarts layer is interactive.
+     * @default true
+     * @since v1.9.0
+     */
+    echartsLayerInteractive?: Boolean
+    /**
+     * Whether to enable large mode
+     * @default false
+     * @since v1.9.0
+     */
+    largeMode?: false
+    /**
+     * Whether echarts layer should be rendered when the map is moving.
+     * if `false`, it will only be re-rendered after the map `moveend`.
+     * It's better to set this option to false if data is large.
+     * @default true
+     */
+    renderOnMoving?: boolean
+  }
+}
+
+export = EChartsExtensionAMapComponent
